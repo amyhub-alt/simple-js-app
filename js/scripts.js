94c16f9852c28171b0e4e3aded81d360aee5bce8
altiefermann <163668836+amyhub-alt@users.noreply.github.com>
2024-05-21 20:51:10 -0400

    removed old work + added pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello world');
-let favoriteFood = 'fried chicky sammo';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: "Bulbasaur",
+        height: 7,
+        types:["grass", "poison"]
+    },
+    {   
+        name: "Charmander",
+        height: 0.6,
+        types:["blaze", "solar-power"]
+    },
+    {   
+        name: "Jiggylypuff",
+        height: 0.5,
+        types:["cute-charm", "friend-guard"]
+    }
+];
