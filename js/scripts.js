c308151c64997ca1d1ca409987332fbcf32087f3
altiefermann <163668836+amyhub-alt@users.noreply.github.com>
2024-06-20 10:58:13 -0400

    part 3

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,12 @@ let pokemonList = [
 ];
 
 for(let i=0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name  + " (height: " + pokemonList[i].height + ")<br> " );
+    if(pokemonList[i].height > 1){
+    document.write(pokemonList[i].name  + " (height: " + pokemonList[i].height + ") -Wow, that's big!<br>");
+    } else {
+    document.write(pokemonList[i].name  + " (height: " + pokemonList[i].height + ")<br>" );
+    }
 }
+
+
+
