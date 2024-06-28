6abe8b1c77a2468d73ce6dc7eafe7d61c7ff9f69
altiefermann <163668836+amyhub-alt@users.noreply.github.com>
2024-06-27 21:25:11 -0400

    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,47 @@
-let pokemonList = [
-    {
-        name: "Bulbasaur",
-        height: 7,
-        types:["grass", "poison"]
-    },
-    {   
-        name: "Charmander",
-        height: 0.6,
-        types:["blaze", "solar-power"]
-    },
-    {   
-        name: "Jigglypuff",
-        height: 0.5,
-        types:["cute-charm", "friend-guard"]
-    } 
-];
 
-// for(let i=0; i < pokemonList.length; i++){
-//     if(pokemonList[i].height > 1){
-//     document.write(pokemonList[i].name  + " (height: " + pokemonList[i].height + ") -Wow, that's big!<br>");
-//     } else {
-//     document.write(pokemonList[i].name  + " (height: " + pokemonList[i].height + ")<br>" );
-//     }
-// }
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+            name: "Bulbasaur",
+            height: 7,
+            types:["grass", "poison"]
+        },
+        {   
+            name: "Charmander",
+            height: 0.6,
+            types:["blaze", "solar-power"]
+        },
+        {   
+            name: "Jigglypuff",
+            height: 0.5,
+            types:["cute-charm", "friend-guard"]
+        } 
+    ];
+
+    return{
+        add:function(pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function(){
+            return pokemonList;
+        }
+    };
+    })();
+
+   console.log(pokemonRepository.getAll());
+   pokemonRepository.add({ name: 'Pikachu' });
+   console.log(pokemonRepository.getAll());
 
 
-pokemonList.forEach(function(pokemon){
-    if (pokemon.height > 1) {
-        document.write(pokemon.name + " (height: " + pokemon.height + ")- Wow, that's big!<br>" );
-    } else {
-        document.write(pokemon.name + " (height: " + pokemon.height + ")<br>")
-    }
-});
-    
+
+    pokemonRepository.getAll().forEach(function(pokemon){
+        if (pokemon.height > 1) {
+            document.write(pokemon.name + " (height: " + pokemon.height + ")- Wow, that's big!<br>" );
+        } else {
+            document.write(pokemon.name + " (height: " + pokemon.height + ")<br>")
+        }
+    });
+
    
 
 
