aece058a7eaba91edb97365efe5852917279fbd2
altiefermann <163668836+amyhub-alt@users.noreply.github.com>
2024-06-27 20:37:46 -0400

    changed to a forEach() function

--- js/scripts.js
+++ js/scripts.js
@@ -16,13 +16,35 @@ let pokemonList = [
     } 
 ];
 
-for(let i=0; i < pokemonList.length; i++){
-    if(pokemonList[i].height > 1){
-    document.write(pokemonList[i].name  + " (height: " + pokemonList[i].height + ") -Wow, that's big!<br>");
+// for(let i=0; i < pokemonList.length; i++){
+//     if(pokemonList[i].height > 1){
+//     document.write(pokemonList[i].name  + " (height: " + pokemonList[i].height + ") -Wow, that's big!<br>");
+//     } else {
+//     document.write(pokemonList[i].name  + " (height: " + pokemonList[i].height + ")<br>" );
+//     }
+// }
+
+
+pokemonList.forEach(function(pokemon){
+    if (pokemon.height > 1) {
+        document.write(pokemon.name + " (height: " + pokemon.height + ")- Wow, that's big!<br>" );
     } else {
-    document.write(pokemonList[i].name  + " (height: " + pokemonList[i].height + ")<br>" );
+        document.write(pokemon.name + " (height: " + pokemon.height + ")<br>")
     }
-}
+});
+    
+   
+
+
+// function favFood(response) {
+//    if (response === undefined){
+//     console.log("Please enter your favorite food!");
+//    } else {
+//     console.log(`Your favorite food is ${response}`)
+//    }
+// }
+
+// favFood("chicken fingers");
 
 
 
